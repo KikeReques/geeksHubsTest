84a200814590f5209d8624871aa95716278df574
KikeReques <94559435+KikeReques@users.noreply.github.com>
2022-07-08 12:43:14 +0200

    Add initial files

---
+++ src/main/java/com/geekshubs/kikereques/geekshubsTest/GeekshubsTestApplication.java
@@ -0,0 +1,13 @@
+package com.geekshubs.kikereques.geekshubsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekshubsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekshubsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekshubs/kikereques/geekshubsTest/GeekshubsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekshubs.kikereques.geekshubsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekshubsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
